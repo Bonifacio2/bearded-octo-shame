9f703e2c83b604f9c6f6d7a18aab5aca7fce9f3b
Bonifacio de Oliveira <bonifacio.segundo@gmail.com>
2015-06-22 18:39:50 -0300

    improved add block test

--- ABC_test.py
+++ ABC_test.py
@@ -7,9 +7,9 @@ class ABCTest(unittest.TestCase):
 	def test_add_block(self):
 		block_set = BlockSet()
 		
-		block_set.add_block()
+		block_set.add_block("AB")
 		
-		self.assertEqual(len(block_set.blocks), 0)
+		self.assertEqual(len(block_set.blocks), 1)
 		
 if __name__ == '__main__':
 	unittest.main()
